2e774b874146f7c19883045ed649425025c3173b
Lakshmi <84883977+bluearrowheadgirl@users.noreply.github.com>
2022-11-16 16:52:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX=0;
+noseY=0;
+leftWristX=0;
+rightWristX=0;
+difference=0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size (550, 500);
+    canvas = createCanvas(550, 500);
+    canvas.position(560, 150);
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose", gotPoses)
+}
+
+function modelLoaded(){
+    console.log("modelLoaded")
+}
+
+function gotPoses(results){
+    if(results.length>0){
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX + " noseY = " + noseY);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+    console.log("leftWristX = " + leftWristX + " rightWristX = " + rightWristX + " difference = " + difference);
+}
+}
+
+function draw(){
+    background("gray");
+    document.getElementById("squareSide").innerHTML = "side of the square is " + difference + "px";
+    fill("red");
+    stroke("blue");
+    square(noseX, noseY, difference,);
+}
